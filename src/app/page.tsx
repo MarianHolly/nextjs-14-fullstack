2ff582fcb957b56f8289064af2c160b0f906e7dd
Stupel <hellomariandev@gmail.com>
2024-05-25 22:08:03 +0200

    layout

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="text-center pt-32 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">Welcome to my blog</h1>
+      <p className="max-w-[750px] mx-auto leading-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo cum adipisci, hic enim eum
+        nisi aperiam molestias, perferendis unde excepturi delectus libero, culpa labore fuga
+        incidunt! Temporibus dolores eveniet fugiat optio vitae!
+      </p>
+    </div>
+  );
+}
