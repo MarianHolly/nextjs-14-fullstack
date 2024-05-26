991c24c34b78a54486957eb904a2be7d35287b8f
Stupel <hellomariandev@gmail.com>
2024-05-26 10:46:47 +0200

    second way of auth

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -1,10 +1,18 @@
 import Form from "@/components/form";
+import { redirect } from "next/navigation";
+import { LogoutLink, getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export default function CreatePost() {
+  // authnetication - first option
+  // const { isAuthenticated } = getKindeServerSession();
+  // if (!(await isAuthenticated())) {
+  //  redirect("api/auth/login?post_login_redirect_url=/create-post")}
+
   return (
     <main className="text-center pt-16">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">Create post</h1>
       <Form />
+      <LogoutLink>Log out</LogoutLink>
     </main>
   );
 }
