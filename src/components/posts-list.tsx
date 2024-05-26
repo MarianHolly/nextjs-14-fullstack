b5d8fffd56bce48aaa78e35a59d2755680c13bb4
Stupel <hellomariandev@gmail.com>
2024-05-26 10:47:33 +0200

    create post

---
+++ src/components/posts-list.tsx
@@ -0,0 +1,21 @@
+//TODO: Fetching Data
+// Server Components (GET requests)
+
+import prisma from "@/lib/db";
+import Link from "next/link";
+
+export default async function PostsList() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const posts = await prisma.post.findMany()
+
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li className="mb-3" key={post.id}>
+          <Link href={`/posts/${post.id}`}>{post.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
